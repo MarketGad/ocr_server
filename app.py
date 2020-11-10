cc59964e2abc37a04de44901bc32281892cc31e1
ahtrahdis7 <sidharthabiki@gmail.com>
2020-11-10 16:59:09 +0530

    feature: integrated ocr with expo server

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from werkzeug.utils import secure_filename
 import os
 import io
 import sys
+import json
+from numpy import asarray 
+import base64
 # from flask.ext.session import Session
 SESSION_TYPE = 'memcache'
 
@@ -34,12 +37,17 @@ nextId = 0
 #         print ("using already set userid[" + str(session['userId']) + "]")
 #     sessionId = session.get('userId', None)
 #     return sessionId
+from base64 import decodestring
 
-def ocr(img):
-    print("inside ocr")
-    path = "UploadFiles/"+img.filename
-    print("img path : " + path)
-    img = cv2.imread(path)
+def ocr(data):
+    # print("inside ocr")
+    # path = "UploadFiles/"+img.filename
+    # print("img path : " + path)
+    with open("UploadFiles/ss.jpeg", "wb") as fh:
+        fh.write(base64.b64decode(data["photo"]))
+
+    # print(data.keys())
+    img = cv2.imread("UploadFiles/ss.jpeg")
     custom_config = r'--oem 3 --psm 6'
     text = pytesseract.image_to_string(img, config=custom_config)
 
@@ -61,30 +69,31 @@ def index():
 def ExtractTextFromOcr():
     # userId = verifySessionId()
     # print(request)
-    
-    print(request.get_json())
 
-    if 'file' not in request.files:
-        print('no file part')
-        flash('No file part')
-        return { "message": "NULL"}
-    file = request.files['file']
-    print('files extracted')
-    print(file.filename)
+    # print(request.data)
+    data = json.loads(request.data)
+    # if 'file' not in request.files:
+    #     print('no file part')
+    #     flash('No file part')
+    #     return { "message": "NULL"}
+    # file = request.files['file']
+    # print('files extracted')
+    # print(file.filename)
 
-    if file.filename == '':
-        flash('No selected file')
-        # return render_template('index.html')
+    # if file.filename == '':
+    #     flash('No selected file')
+    #     # return render_template('index.html')
 
     result = ""
+    # print(data)
 
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # props["img"] = file
-        result = ocr(file)
+    # if file and allowed_file(file.filename):
+    #     filename = secure_filename(file.filename)
+    #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #     # props["img"] = file
+    result = ocr(data)
 
-    # print(result)
+    print(result)
     return result
 
 if __name__ == "__main__":
